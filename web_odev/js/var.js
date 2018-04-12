90fdc16588c5037f79012e3e3e820ef1165e69db
İrem Sıla Akıllılar <32552162+iremakillilar@users.noreply.github.com>
2018-04-13 00:54:22 +0300

    Add files via upload

---
+++ web_odev/js/var.js
@@ -0,0 +1,44 @@
+var menu = '\
+<a class="py-2 d-none d-md-inline-block" href="index.html">Ana Sayfa</a>\
+<a class="py-2 d-none d-md-inline-block" href="endustri4.0.html">Endüstri 4.0 </a>\
+<a class="py-2 d-none d-md-inline-block" href="4kullanim.html">Endüstri 4.0 Kullanım Alanları</a>\
+<a class="py-2 d-none d-md-inline-block" href="endustri5.0.html">Endüstri 5.0 </a>\
+<a class="py-2 d-none d-md-inline-block" href="5kullanim.html">Nesnelerin İnterneti</a>\
+<a class="py-2 d-none d-md-inline-block" href="yapayzeka.html">Yapay Zeka 101</a>' ;
+
+var ayak='\
+<footer class="container py-5">\
+<div class="row">\
+<div class="col-12 col-md">\
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="1794"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>\
+<small class="d-block mb-3 text-muted">&copy; 2017-2018</small>\
+</div>\
+<div class="col-6 col-md">\
+<ul class="list-unstyled text-small">\
+<li><a class="text-muted" href="endustri4.0.html">Endüstri 4.0 </a></li>\
+</ul>\
+</div>\
+<div class="col-6 col-md">\
+<ul class="list-unstyled text-small">\
+<li><a class="text-muted" href="4kullanim.html">Endüstri 4.0 Kullanım Alanları</a></li>\
+</ul>\
+</div>\
+<div class="col-6 col-md">\
+<ul class="list-unstyled text-small">\
+<li><a class="text-muted" href="endustri5.0.html">Endüstri 5.0</a></li>\
+</ul>\
+</div>\
+<div class="col-6 col-md">\
+<ul class="list-unstyled text-small">\
+<li><a class="text-muted" href="5kullanim.html">Nesnelerin İnterneti</a></li>\
+</ul>\
+</div>\
+<div class="col-6 col-md">\
+<ul class="list-unstyled text-small">\
+<li><a class="text-muted" href="yapayzeka.html">Yapay Zeka 101</a></li>\
+</ul>\
+</div>\
+</div>\
+</footer>' ;
+
+ 
